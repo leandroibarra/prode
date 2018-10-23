19d6d32eccf1acf646cb4e23962c58ab67cbb973
Leandro Ibarra <lea14_9@hotmail.com>
2018-10-23 19:35:17 -0300

    Remove obsolete sidebar section and files.

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -15,7 +15,6 @@
     <link href="{{ asset('css/game.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/circliful.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" type="text/css" />
-    {{--<link href="{{ asset('css/sidebar.css') }}" rel="stylesheet" type="text/css" />--}}
 </head>
 <body>
     @include('game.partials.topnav')
@@ -26,12 +25,10 @@
 
     @include('game.partials.footer')
 
-
     <!-- Scripts -->
     <script src="{{ asset('js/jquery-3.3.1.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/bootstrap-4.1.3.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/circliful.js') }}" type="text/javascript"></script>
-    {{--<script src="{{ asset('js/sidebar.js') }}" type="text/javascript"></script>--}}
     @yield ('scripts')
 </body>
 </html>
